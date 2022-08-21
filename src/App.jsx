39367f3e4f702292444f6b51751a607346758fe6
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-08-21 14:23:50 +0300

    single thread message

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import React from 'react';//
 import { useAuthContext } from './hooks/useAuthContext';
 import { NavBar } from './components/navBar/navBar';
 import { ChatForm } from './components/ChatForm/ChatForm';
-import { useCollection } from './hooks/useCollection';
 import { Chat } from './components/Chat/Chat';
 import './App.scss';
 
 export const App = () => {
-  const { document } = useCollection('chat-text');
   const { user } = useAuthContext();
 
   return (
@@ -16,7 +14,7 @@ export const App = () => {
       {
         user && (
           <>
-            <Chat document={document} />
+            <Chat />
             <ChatForm />
           </>
         )
